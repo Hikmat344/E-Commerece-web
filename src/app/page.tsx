db65efdd12d85c4e405d60605955c2cbb00b29bc
Hikmat Ullah <hikmatcse1919@gmail.com>
2023-06-03 03:59:47 +0500

    footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import { Difference } from '../../components/section/Difference'
 import { Footer } from '../../components/section/Footer'
 import { Hero } from '../../components/section/Hero'
-
 import Newsletter from '../../components/section/Newsletter'
 import Products from '../../components/section/Products'
 import Promotion from '../../components/section/Promotion'
